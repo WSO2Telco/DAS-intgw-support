cf64bdbd3aae50a20b53bc23185b6f2e15e5c94b
HPBmla <bimlamadhavee@gmail.com>
2017-12-19 00:14:12 +0530

    Fixation for INTGWAN-319

--- features/hub-authentication-artifacts/hub-authentication-analytics/dashboard/HubAnalytics-Gadget-TrafficReport_1.0.0/HubAnalytics-Gadget-TrafficReport/js/core/gadget-core.js
+++ features/hub-authentication-artifacts/hub-authentication-analytics/dashboard/HubAnalytics-Gadget-TrafficReport_1.0.0/HubAnalytics-Gadget-TrafficReport/js/core/gadget-core.js
@@ -42,6 +42,7 @@ $(function () {
                 conf.dateStart = moment(moment($("#reportrange").text().split("-")[0]).format("MMMM D, YYYY hh:mm A")).valueOf();
                 conf.dateEnd = moment(moment($("#reportrange").text().split("-")[1]).format("MMMM D, YYYY hh:mm A")).valueOf();
                 if($("#button-type").val().toLowerCase().trim() == "error traffic") {
+
                     conf["provider-conf"].tableName = "ORG_WSO2TELCO_ANALYTICS_HUB_STREAM_FAILURE_SUMMARY_PER_DAY";
                 } else {
                     conf["provider-conf"].tableName = "ORG_WSO2TELCO_ANALYTICS_HUB_STREAM_TRAFFIC_SUMMARY_PER_DAY";
